94a66e5ec8f19a844afd06deb1140edf306bddba
Galip PALA <galip.pala@tompay.com.tr>
2022-09-10 20:50:04 +0300

    git try fix.

--- e-wallet/src/main/java/com/kn/ewallet/EWalletApplication.java
+++ e-wallet/src/main/java/com/kn/ewallet/EWalletApplication.java
@@ -7,11 +7,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info =  @Info(title = "Wallet API", version = "1.0", description = "Wallet API Docs"))
+@OpenAPIDefinition(info = @Info(title = "Wallet API", version = "1.0", description = "Wallet API Docs"))
 public class EWalletApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(EWalletApplication.class, args);
+		SpringApplication.run(EWalletApplication.class, args);
 	}
 
 }
